c4c1ac3a85f7d52b7f89466890939f5ffe93fc89
Davi Mattos <contato.davimattos14@gmail.com>
2024-11-04 20:16:29 -0300

    2024-11-04 20:16:28

--- Arquitetura de Computadores e Sistemas Operacionais/Trabalho 1 de ACSO 2024-  1 - Grupo 5/escalonador.c
+++ Arquitetura de Computadores e Sistemas Operacionais/Trabalho 1 de ACSO 2024-  1 - Grupo 5/escalonador.c
@@ -1,31 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define NUM_PROCESSOS 10
+#define MAX 256
+#define QUANTUM 5
 
-int main(){
+typedef struct
+{
+    char processo[3];
+    int t_chegada;
+    int prioridade;
+    int t_servico;
+} Processo;
 
-    FILE *fp = fopen("Tabela_Processos.csv", "r");
-    if (!fp) {
+int lerCSV(Processo p[])
+{
+    FILE *arq = fopen("processos.csv", "r");
+    if (arq == NULL)
+    {
         printf("Não é possível abrir o arquivo\n");
         return 1;
     }
 
-    char buf[1024];
-    while (fgets(buf, 1024, fp)) {
-        char *campo = strtok(buf, ",");
-        while(campo) {
-            printf("%s\n", campo);
-            campo = strtok(NULL, ",");
-        }
+    char linha[MAX];
+    int i = 0;
+
+    fgets(linha, MAX, arq);
+
+    while (!feof(arq))
+    {
+        fgets(linha, MAX, arq);
+        char *leitura = strtok(linha, ";");
+        strcpy(p[i].processo, leitura);
+
+        leitura = strtok(NULL, ";");
+        p[i].t_chegada = atoi(leitura);
+
+        leitura = strtok(NULL, ";");
+        p[i].prioridade = atoi(leitura);
+
+        leitura = strtok(NULL, ";");
+        p[i].t_servico = atoi(leitura);
+        i++;
     }
+    fclose(arq);
+    return i;
+}
 
+int main()
+{
+    Processo processos[MAX];
+    int n = lerCSV(processos);
 
-// Create a file
-    FILE *fptr = fopen("saida.txt", "w");
-
-// Write some text to the file
-    fprintf(fptr, buf); 
-
-// Close the file
-    fclose(fp);
-    fclose(fptr);
+    printf("Processo: %s \tTempo de Chegada: %d\tPrioridade: %d\tTempo de Execucao: %d",
+           processos[2].processo, processos[2].t_chegada, processos[2].prioridade, processos[2].t_servico);
+    /*
+     */
     return 0;
 }
