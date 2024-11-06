09852cc589b027b05d028737a36ea031c91cc244
Davi Mattos <davimattos2012@gmail.com>
2024-11-05 21:35:43 -0300

    2024-11-05 21:35:43

--- Arquitetura de Computadores e Sistemas Operacionais/Trabalho 1 de ACSO 2024-  1 - Grupo 5/escalonador.c
+++ Arquitetura de Computadores e Sistemas Operacionais/Trabalho 1 de ACSO 2024-  1 - Grupo 5/escalonador.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #define NUM_PROCESSOS 5
 #define MAX 256
-#define QUANTUM 3
+#define QUANTUM 5
 
 typedef struct
 {
@@ -13,6 +13,9 @@ typedef struct
     int t_servico;
 } Processo;
 
+/*
+    FAZ A LEITURA DO ARQUIVO .CSV E SALVA EM UM STRUCT
+*/
 int lerCSV(Processo p[])
 {
     FILE *arq = fopen("processos.csv", "r");
@@ -46,6 +49,10 @@ int lerCSV(Processo p[])
     fclose(arq);
     return i;
 }
+
+/*
+    ATUALIZA A FILA DE PROCESSOS APÓS O PROCESSO EM EXECUÇÃO TERMINAR (FIM DO TIME SLICE OU FIM DE PROCESSO)
+*/
 void atualiza_fila(Processo p[], int processo_em_execucao)
 {
     Processo aux = p[0];
@@ -53,24 +60,31 @@ void atualiza_fila(Processo p[], int processo_em_execucao)
     {
         p[i] = p[i + 1];
     }
-    p[processo_em_execucao - 1] = aux;    
+    p[processo_em_execucao - 1] = aux;
 }
 
-void imprimir(Processo p[], int processo_em_execucao){
+/*
+    IMPRIMIR TABELA DE PROCESSOS
+*/
+void imprimir(Processo p[], int processo_em_execucao)
+{
     for (int i = 0; i < processo_em_execucao; i++)
     {
         printf("Processo: %s \tTempo de Execucao: %d\tTempo de Chegada: %d\tPrioridade: %d\n",
-                p[i].processo, p[i].t_servico, p[i].t_chegada, p[i].prioridade);
+               p[i].processo, p[i].t_servico, p[i].t_chegada, p[i].prioridade);
     }
     printf("\n\n");
 }
 
+/*
+    VERIFICA A CHEGADA DE UM NOVO PROCESSO E O COLOCA NA FILA
+*/
 int chegada(Processo p[], Processo fila[], int ut, int processos_em_execucao, int tempo_execucao)
 {
     int aux = processos_em_execucao;
     for (int i = 0; i < NUM_PROCESSOS; i++)
     {
-        if (ut >= p[i].t_chegada && p[i].t_chegada >= (ut - tempo_execucao))
+        if (ut == p[i].t_chegada || (ut > p[i].t_chegada && p[i].t_chegada > (ut - tempo_execucao)))
         {
             fila[aux] = p[i];
             aux++;
@@ -79,6 +93,9 @@ int chegada(Processo p[], Processo fila[], int ut, int processos_em_execucao, in
     return aux;
 }
 
+/*
+    EXECUTA O ALGORITMO DE ROUND ROBIN COM QUANTUM = 5 E SALVA A SAÍDA EM UM .TXT
+*/
 void round_robin(Processo p[])
 {
     int ut = 0;
@@ -92,18 +109,16 @@ void round_robin(Processo p[])
     char linha[MAX];
     while (1)
     {
-        imprimir(fila, processos_em_execucao);
+        // imprimir(fila, processos_em_execucao);   -   IMPRIMIR A FILA NO PROMPT
         tempo_execucao++;
         ut++;
         fila[0].t_servico--;
-        //processos_em_execucao = chegada(p, fila, ut, processos_em_execucao);
         if (fila[0].t_servico == 0)
         {
-            sprintf(linha, "*U.T= %d|\t O Processo %s executou por %ds e Terminou\n", (ut - tempo_execucao), fila[0].processo, tempo_execucao);
+            sprintf(linha, "U.T= %d~%d|\t O Processo %s executou por %ds e Terminou\n", (ut - tempo_execucao), ut, fila[0].processo, tempo_execucao);
             fputs(linha, saida);
             if (processos_em_execucao > 1)
             {
-                printf("UT - %d\n", ut);
                 atualiza_fila(fila, processos_em_execucao);
             }
             tempo_execucao = 0;
@@ -113,16 +128,16 @@ void round_robin(Processo p[])
                 fclose(saida);
                 break;
             }
+
             continue;
         }
         if (tempo_execucao == QUANTUM)
         {
             processos_em_execucao = chegada(p, fila, ut, processos_em_execucao, tempo_execucao);
-            sprintf(linha, "U.T= %d|\t O Processo %s executou por %ds e foi para Baixa prioridade\n", (ut - tempo_execucao), fila[0].processo, tempo_execucao);
+            sprintf(linha, "U.T= %d~%d|\t O Processo %s executou por %ds e foi para Baixa prioridade\n", (ut - tempo_execucao), ut, fila[0].processo, tempo_execucao);
             fputs(linha, saida);
             if (processos_em_execucao > 1)
             {
-                printf("UT - %d\n", ut);
                 atualiza_fila(fila, processos_em_execucao);
             }
 
@@ -131,13 +146,16 @@ void round_robin(Processo p[])
     }
 }
 
+/*
+    FUNÇÃO PRINCIPAL, CRIA O ARQUIVO SAIDA.TXT, SALVA A TABELA E CHAMA O ALGORITMO
+*/
 int main()
 {
     Processo processos[NUM_PROCESSOS];
     int n = lerCSV(processos);
     FILE *saida = fopen("Saida.txt", "w+");
     char linha[MAX];
-    sprintf(linha, "\tNum Processsos:%d\n", n);
+    sprintf(linha, "\tNum Processsos:%d\t\tQuantum: %d\n", n, QUANTUM);
     fputs(linha, saida);
     for (int i = 0; i < n; i++)
     {
